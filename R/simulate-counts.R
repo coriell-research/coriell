c0b8950b82f3a5f3c5a3a55a53865e616487eb98
jcalendo <jcalendo@gmail.com>
2020-08-20 16:43:41 -0400

    Added function for simulating RNAseq counts

---
+++ R/simulate-counts.R
@@ -0,0 +1,55 @@
+#' Simulate RNASeq Count Data
+#' 
+#' Simulate an RNASeq count matrix. Adapted from edgeR documentation.
+#' @param n_genes integer. The number of gene to simulate
+#' @param n_de integer. The number of genes to be differentially expressed
+#' @param n_samples integer. The number of samples to simulate
+#' @param groups character vector. Character vector specifying the names of the groups to simulate
+#' @param de_group character. The name of the group to be differentially expressed
+#' @param mu numeric. Mean value of counts
+#' @param phi numeric. Dispersion parameter
+#' @param count_offset numeric. Count to be added to the de_group
+#' @return list containing the count matrix and a vector indicating which rows were modified.
+#' @examples
+#' # simulate counts using default parameters
+#' sim <- simulate_counts()
+#' 
+#' # extract the count matrix from the simulation result
+#' counts <- sim$table
+#' 
+#' # show rows where counts were modified
+#' counts[sim$de_genes, ]
+#' @export
+simulate_counts = function(n_genes = 1000, 
+                           n_de = 100, 
+                           n_samples = 6, 
+                           groups = c("ctl", "trt"),
+                           de_group = "trt", 
+                           mu = 10, 
+                           phi = 0.1, 
+                           count_offset = 25) {
+  
+  stopifnot("de_group must be a member of groups" = de_group %in% groups)
+  stopifnot("More groups than samples" = length(groups) <= n_samples)
+  stopifnot("n_samples not a multiple of levels(groups)" = n_samples %% length(groups) == 0)
+  
+  # randomly select rows of genes to be differentially expressed
+  de_genes <- sample(n_genes, size = n_de)
+  
+  # generate the count table
+  counts <- matrix(rnbinom(n_genes * n_samples,
+                           mu = mu,
+                           size = 1 / phi),
+                  nrow = n_genes,
+                  ncol = n_samples)
+  
+  colnames(counts) <- rep(groups, each = n_samples / length(groups))
+  
+  group <- gl(length(groups), n_samples / length(groups), labels = groups)
+  counts[de_genes, group == de_group] <- counts[de_genes, group == de_group] + count_offset
+  
+  rownames(counts) <- paste("gene", 1:n_genes, sep = ".")
+  
+  list("table" = counts, 
+       "de_genes" = paste("gene", de_genes, sep = "."))
+}
