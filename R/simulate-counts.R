67458ae9bc06966d930da2f0451e04b11f215eb0
jcalendo <jcalendo@gmail.com>
2020-08-21 11:30:28 -0400

    allowed for up/down genes in simulated counts

--- R/simulate-counts.R
+++ R/simulate-counts.R
@@ -2,14 +2,15 @@
 #' 
 #' Simulate an RNASeq count matrix. Adapted from edgeR documentation.
 #' @param n_genes integer. The number of gene to simulate
-#' @param n_de integer. The number of genes to be differentially expressed
+#' @param n_up integer. The number of genes the count offset will be added to
+#' @param n_down integer. the number of genes the count offset will be subtracted from. Negative numbers will be coerced to zeros.
 #' @param n_samples integer. The number of samples to simulate
 #' @param groups character vector. Character vector specifying the names of the groups to simulate
 #' @param de_group character. The name of the group to be differentially expressed
 #' @param mu numeric. Mean value of counts
 #' @param phi numeric. Dispersion parameter
-#' @param count_offset numeric. Count to be added to the de_group
-#' @return list containing the count matrix and a vector indicating which rows were modified.
+#' @param count_offset numeric. Count offset to be applied to the up/down genes.
+#' @return list containing the count matrix and a vectors indicating which rows were modified.
 #' @examples
 #' # simulate counts using default parameters
 #' sim <- simulate_counts()
@@ -21,20 +22,22 @@
 #' counts[sim$de_genes, ]
 #' @export
 simulate_counts = function(n_genes = 1000, 
-                           n_de = 100, 
+                           n_up = 50,
+                           n_down = 50,
                            n_samples = 6, 
                            groups = c("ctl", "trt"),
                            de_group = "trt", 
                            mu = 10, 
                            phi = 0.1, 
-                           count_offset = 25) {
+                           count_offset = 10) {
   
   stopifnot("de_group must be a member of groups" = de_group %in% groups)
   stopifnot("More groups than samples" = length(groups) <= n_samples)
   stopifnot("n_samples not a multiple of levels(groups)" = n_samples %% length(groups) == 0)
   
-  # randomly select rows of genes to be differentially expressed
-  de_genes <- sample(n_genes, size = n_de)
+  de_genes <- sample(n_genes, size = n_up + n_down)
+  up <- sample(de_genes, size = n_up)
+  down <- setdiff(de_genes, up)
   
   # generate the count table
   counts <- matrix(stats::rnbinom(n_genes * n_samples,
@@ -46,10 +49,16 @@ simulate_counts = function(n_genes = 1000,
   colnames(counts) <- rep(groups, each = n_samples / length(groups))
   
   group <- gl(length(groups), n_samples / length(groups), labels = groups)
-  counts[de_genes, group == de_group] <- counts[de_genes, group == de_group] + count_offset
+  
+  counts[up, group == de_group] <- counts[up, group == de_group] + count_offset
+  counts[down, group == de_group] <- counts[down, group == de_group] - count_offset
+  
+  # silently convert negative numbers to zero if present
+  counts <- apply(counts, 2, function(x) ifelse(x < 0, 0, x))
   
   rownames(counts) <- paste("gene", 1:n_genes, sep = ".")
   
   list("table" = counts, 
-       "de_genes" = paste("gene", de_genes, sep = "."))
+       "up_genes" = paste("gene", up, sep = "."),
+       "down_genes" = paste("gene", down, sep = "."))
 }
