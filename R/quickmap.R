4e70c4719d631b7892bbe11e791c95cab444f23b
jcalendo <jcalendo@gmail.com>
2020-10-14 17:31:22 -0400

    Added quickmap function

---
+++ R/quickmap.R
@@ -0,0 +1,27 @@
+#' Quick Heatmap with Sensible Defaults for RNA-seq
+#'
+#' Generate a heatmap using pheatmap with sensible defaults for RNA-seq.
+#' Utilizes a default blue to red diverging color palette, removes rownames
+#' and borders, clusters rows and columns, sets row-wise clustering is euclidean
+#' column-wise clustering is correlation with clustering method set to complete.
+#' Display font size is reduced and column labels are set to an angle. Defaults can
+#' be overridden and/or any additional arguments can be passed to the pheatmap
+#' function via ...
+#'
+#' @importFrom pheatmap pheatmap
+#' @importFrom grDevices colorRampPalette
+#' @param mat numeric matrix to be passed onto pheatmap function
+#' @param ... args to be passed to pheatmap function
+#' @return pheatmap object. See ?pheatmap::pheatmap for details
+#' @export
+quickmap <- function(mat, ...) {
+  col_code <- (grDevices::colorRampPalette(c("dodgerblue3", "grey99", "firebrick3")))(50)
+  default_args <- list(
+    scale = "row", show_rownames = FALSE, border_color = NA, cluster_rows = TRUE,
+    cluster_cols = TRUE, color = col_code, cellWidth = 20, treeheight_row = 0, clustering_distance_rows = "euclidean",
+    clustering_distance_cols = "correlation", clustering_method = "complete", fontsize_col = 8, angle_col = 45
+  )
+  user_args <- list(...)
+  default_args[names(user_args)] <- user_args
+  do.call(pheatmap::pheatmap, c(list(mat = mat), default_args))
+}
