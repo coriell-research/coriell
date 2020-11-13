078f023db0d1859713009030a57eea202bab329a
jcalendo <jcalendo@gmail.com>
2020-11-12 22:02:28 -0500

    Use utilize to create query url. Included all datasets for query. Removed default biological_process.

--- R/panther-go.R
+++ R/panther-go.R
@@ -1,86 +1,117 @@
 #' Perform GO Analysis with PANTHER
 #'
-#' Sends a request to PANTHER GO db to perform over representation analysis. This function 
+#' Sends a request to PANTHER GO db to perform over representation analysis. This function
 #' excludes the option to import a reference list and reference organism. By default,
-#' in this case, PANTHER will use all of the genes of the given organism as the 
+#' in this case, PANTHER will use all of the genes of the given organism as the
 #' reference list.
-#' @param gene_list character vector. Maximum of 100,000 Identifiers. Can be any 
-#' of the following: Ensemble gene identifier, Ensemble protein identifier, 
-#' Ensemble transcript identifier, Entrez gene id, gene symbol, NCBI GI, HGNC Id, 
-#' International protein index id, NCBI UniGene id, UniProt accession and 
-#' UniProt id 
-#'@param organism character string. Taxon ID (e.g. "9606" for HUMAN, "10090" for 
-#'MOUSE, "10116" for RAT). To get list of available taxon IDs see:
-#'`curl -X GET "http://pantherdb.org/services/oai/pantherdb/supportedgenomes" -H  "accept: application/json"` 
-#'@param annot_dataset character string. One of c("biological_process", 
-#'"molecular_function", "cellular_component"). 
-#'@param enrichment_test_type character string. One of c("fisher", "binomial").
-#'Default "fisher"
-#'@param correction character string. One of c("fdr", "bonferroni", "none").
-#'Default "fdr"
-#'@return list with results of GO analysis and input information. See PANTHER user manual 
-#'\url{http://www.pantherdb.org/help/PANTHER_user_manual.pdf} for details
-#'@export
-#'@examples
+#'
+#' <http://pantherdb.org/services/tryItOut.jsp?url=%2Fservices%2Fapi%2Fpanther> states
+#' that 100,000 identifiers can be submitted to the service however some very
+#' large requests (> 1,000 genes) have been returning a 400 error from the PANTHER API
+#' stating that the request url is too long. If you encounter an error with this
+#' function, first try reducing the size of your input list.
+#'
+#' @param gene_list character vector. Maximum of 100,000 identifiers. Can be any
+#' of the following: Ensemble gene identifier, Ensemble protein identifier,
+#' Ensemble transcript identifier, Entrez gene id, gene symbol, NCBI GI, HGNC Id,
+#' International protein index id, NCBI UniGene id, UniProt accession and
+#' UniProt id
+#' @param organism character string. Taxon ID (e.g. "9606" for HUMAN, "10090" for
+#' MOUSE, "10116" for RAT). To get list of available taxon IDs see:
+#' `curl -X GET "http://pantherdb.org/services/oai/pantherdb/supportedgenomes" -H  "accept: application/json"`
+#' @param annot_dataset character string. One of c("biological_process",
+#' "molecular_function", "cellular_component", "panther_go_slim_mf", "panther_go_slim_bp",
+#' "panther_go_slim_cc", "panther_pc", "panther_pathway", "panther_reactome_pathway"). see:
+#' `curl -X POST "http://pantherdb.org/services/oai/pantherdb/supportedannotdatasets" -H "accept: application/json"`
+#' for full descriptions.
+#' @param enrichment_test_type character string. One of c("fisher", "binomial").
+#' Default "fisher"
+#' @param correction character string. One of c("fdr", "bonferroni", "none").
+#' Default "fdr"
+#' @return list with results of GO analysis and input information. See PANTHER user manual
+#' \url{http://www.pantherdb.org/help/PANTHER_user_manual.pdf} for details.
+#' @export
+#' @examples
 #' library(coriell)
 #'
 #'
-#' genes <- c("CTNNB1", "ADAM17", "AXIN1", "AXIN2", "CCND2", "CSNK1E", "CTNNB1", 
-#'            "CUL1", "DKK1", "DKK4", "DLL1", "DVL2", "FRAT1", "FZD1", "FZD8", 
-#'            "GNAI1", "HDAC11", "HDAC2", "HDAC5", "HEY1", "HEY2", "JAG1", 
-#'            "JAG2", "KAT2A", "LEF1", "MAML1", "MYC", "NCOR2", "NCSTN", 
-#'            "NKD1", "NOTCH1", "NOTCH4", "NUMB", "PPARD", "PSEN2", "PTCH1", 
-#'            "RBPJ", "SKP2", "TCF7", "TP53", "WNT1", "WNT5B", "WNT6")
+#' genes <- c(
+#'   "CTNNB1", "ADAM17", "AXIN1", "AXIN2", "CCND2", "CSNK1E", "CTNNB1",
+#'   "CUL1", "DKK1", "DKK4", "DLL1", "DVL2", "FRAT1", "FZD1", "FZD8",
+#'   "GNAI1", "HDAC11", "HDAC2", "HDAC5", "HEY1", "HEY2", "JAG1",
+#'   "JAG2", "KAT2A", "LEF1", "MAML1", "MYC", "NCOR2", "NCSTN",
+#'   "NKD1", "NOTCH1", "NOTCH4", "NUMB", "PPARD", "PSEN2", "PTCH1",
+#'   "RBPJ", "SKP2", "TCF7", "TP53", "WNT1", "WNT5B", "WNT6"
+#' )
 #'
 #' go_results <- panther_go(genes, organism = "9606", annot_dataset = "biological_process")
-#' 
+#'
 #' # to view results access the 'results' item
 #' head(go_results$results)
 panther_go <- function(
                        gene_list,
                        organism,
-                       annot_dataset = "biological_process",
+                       annot_dataset,
                        enrichment_test_type = "fisher",
                        correction = "fdr") {
-  datasets <- c("biological_process" = "GO%3A0008150", 
-                "molecular_function" = "GO%3A0003674",
-                "cellular_component" = "GO%3A0005575")
+  datasets <- c(
+    "biological_process" = "GO:0008150",
+    "molecular_function" = "GO:0003674",
+    "cellular_component" = "GO:0005575",
+    "panther_go_slim_mf" = "ANNOT_TYPE_ID_PANTHER_GO_SLIM_MF",
+    "panther_go_slim_bp" = "ANNOT_TYPE_ID_PANTHER_GO_SLIM_BP",
+    "panther_go_slim_cc" = "ANNOT_TYPE_ID_PANTHER_GO_SLIM_CC",
+    "panther_pc" = "ANNOT_TYPE_ID_PANTHER_PC",
+    "panther_pathway" = "ANNOT_TYPE_ID_PANTHER_PATHWAY",
+    "panther_reactome_pathway" = "ANNOT_TYPE_ID_REACTOME_PATHWAY"
+  )
   tests <- c("fisher" = "FISHER", "binomial" = "BINOMIAL")
   corrections <- c("fdr" = "FDR", "bonferroni" = "BONFERRONI", "none" = "NONE")
-  
+
   stopifnot("Empty gene_list" = length(gene_list) > 0)
   stopifnot("Too many genes in gene list" = length(gene_list) <= 100000)
   stopifnot("Only one organism identifier should be provided" = length(organism) == 1)
-  stopifnot("annot_dataset must be one of c('biological_process', 'molecular_function', 'cellular_component')" = annot_dataset %in% names(datasets))
+  stopifnot("Incorrectly specified dataset" = annot_dataset %in% names(datasets))
   stopifnot("enrichment_test_type must be one of c('fisher', 'binomial')" = enrichment_test_type %in% names(tests))
   stopifnot("correction must be one of c('fdr', 'bonferroni', 'none')" = correction %in% names(corrections))
-  
+
   base_url <- "http://pantherdb.org/services/oai/pantherdb/enrich/overrep?"
-  gene_input <- paste0("geneInputList=", paste(gene_list, collapse = ","))
-  organism_input <- paste0("&organism=", organism)
-  annot_input <- paste0("&annotDataSet=", datasets[annot_dataset])
-  test_input <- paste0("&enrichmentTestType=", tests[enrichment_test_type])
-  correction_input <- paste0("&correction=", corrections[correction])
-  req_url <- paste0(base_url, gene_input, organism_input, annot_input, test_input, correction_input)
-  
-  r <- httr::GET(req_url)
+  gene_input <- paste(gene_list, collapse = ",")
+  organism_input <- organism
+  annot_input <- datasets[annot_dataset]
+  test_input <- tests[enrichment_test_type]
+  correction_input <- corrections[correction]
+  req_url <- httr::modify_url(
+    base_url,
+    query = list(
+      geneInputList = gene_input,
+      organism = organism_input,
+      annotDataSet = annot_input,
+      enrichmentTestType = test_input,
+      correction = correction_input
+    )
+  )
+
+  r <- httr::GET(req_url, httr::accept_json())
   httr::stop_for_status(r, "ERROR: failed to execute request")
   httr::warn_for_status(r, "WARNING: request produced a warning response")
-  
+
   parsed <- jsonlite::fromJSON(httr::content(r, "text"), simplifyVector = FALSE)
-  
-  results <- purrr::pluck(parsed, "results", "result") %>% 
-    purrr::map(dplyr::as_tibble) %>% 
-    dplyr::bind_rows(.id = "result_number") %>% 
+  results <- purrr::pluck(parsed, "results", "result") %>%
+    purrr::map(dplyr::as_tibble) %>%
+    dplyr::bind_rows(.id = "result_number") %>%
     tidyr::unnest(term) %>%
-    dplyr::group_by(result_number) %>% 
-    dplyr::mutate(ind = rep(c("GO_term", "description"), length.out = dplyr::n())) %>% 
+    dplyr::group_by(result_number) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(ind = rep(c("GO_term", "description"), length.out = dplyr::n())) %>%
     tidyr::pivot_wider(names_from = ind, values_from = term)
-  
-  list("results" = results,
-       "gene_query" = gene_list,
-       "organism_query" = organism,
-       "test_type" = tests[enrichment_test_type],
-       "correction" = corrections[correction],
-       "request_url" = req_url)
+
+  list(
+    "results" = results,
+    "gene_query" = gene_list,
+    "organism_query" = organism,
+    "test_type" = tests[enrichment_test_type],
+    "correction" = corrections[correction],
+    "request_url" = req_url
+  )
 }
