27e9d30838ccd28b598729112262a9d0e586eece
jcalendo <jcalendo@gmail.com>
2021-08-04 16:11:32 -0400

    Added distinct color palette generating function

--- R/colors.R
+++ R/colors.R
@@ -1,8 +1,8 @@
-#' Generate Random RGB Color Palette
-#' 
-#' This function will randomly generate N colors from the rgb colorspace. It 
+#' Generate random RGB color palette
+#'
+#' This function will randomly generate N colors from the rgb colorspace. It
 #' makes no attempt at ensuring the colors are distinct or well separated.
-#' 
+#'
 #' @param n numeric. Number of random colors to generate
 #' @param alpha numeric. Transparency level of color palette (0-1). Default 1.0
 #' @export
@@ -11,9 +11,38 @@ random_rgb_palette <- function(n, alpha = 1.0) {
   stopifnot("n must be numeric" = is.numeric(n))
   stopifnot("length(n) must be equal to 1" = length(n) == 1)
   stopifnot("alpha must be between 0 and 1" = alpha >= 0 & alpha <= 1)
-  
-  grDevices::rgb(red = stats::runif(n), 
-                 green = stats::runif(n), 
-                 blue = stats::runif(n), 
-                 alpha = alpha)
+
+  grDevices::rgb(
+    red = stats::runif(n),
+    green = stats::runif(n),
+    blue = stats::runif(n),
+    alpha = alpha
+  )
+}
+
+#' Generate a distinct RGB color palette
+#'
+#' This function uses `kmeans()` over the RGB colorspace to generate N distinct
+#' RGB colors. It's usually not a problem if `kmeans()` does not converge (colors
+#' are distinct enough for most purposes). If you get warnings, you can modify
+#' the default settings of the `kmeans()` function by passing in additional
+#' arguments. For example, increasing iterations can be done by passing
+#' `iter.max = 100`. Changing the kmeans algorithm can be done by specifying
+#' `algorithm = "Lloyd"`. Changing these arguments may eliminate warnings.
+#' @param n numeric. Number of colors to generate
+#' @param alpha numeric. Transparency level of the color palette (0-1). Default 1.0
+#' @param ... arguments passed to `kmeans()`
+#' @export
+#' @return vector of distinct RGB colors
+distinct_rgb_palette <- function(n, alpha = 1.0, ...) {
+  stopifnot("n must be numeric" = is.numeric(n))
+  stopifnot("length(n) must be equal to 1" = length(n) == 1)
+  stopifnot("alpha must be between 0 and 1" = alpha >= 0 & alpha <= 1)
+
+  space <- expand.grid(rep(list(0:255), 3))
+  res <- stats::kmeans(space, centers = n, ...)
+  centers <- floor(res$centers)
+  pal <- apply(centers, 1, function(x) rgb(x[1], x[2], x[3], maxColorValue = 255, alpha = (255 * alpha)))
+
+  return(pal)
 }
