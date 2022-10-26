210a7f935372a16e9a16a2f99b15f2e8ed37db35
jcalendo <jcalendo@gmail.com>
2022-10-26 16:42:58 -0400

    Update docs to use builtin data and round results

--- R/summarize-dge.R
+++ R/summarize-dge.R
@@ -11,29 +11,7 @@
 #' @export
 #' @return data.frame with columns describing the count and percentages of up/down/unperturbed genes
 #' @examples
-#' library(coriell)
-#' 
-#' 
-#' # Create a simulated results data.frame
-#' # 50 will have logFC > 0
-#' # 50 will have logFC < 0
-#' # 25 of the up-regulated will have significant FDR values (< 0.0001)
-#' # 25 of the down-regulated will have significant FDR values (< 0.0001)
-#' df <- data.frame(feature_id = paste("gene", 1:100,sep = "."),
-#'                  logFC = c(runif(50, min = -8, max = -0.01), 
-#'                            runif(50, min = 0.01, max = 8)),
-#'                  FDR = c(runif(25, min = 1e-12, max = 1e-4),
-#'                          runif(25, min = 0.06, max = 1),
-#'                          runif(25, min = 1e-12, max = 1e-4),
-#'                          runif(25, min = 0.06, max = 1))
-#' )
-#' 
-#' # view the results data.frame
-#' head(df)
-#' 
-#' # summarize results
-#' summarize_dge(df)
-#' 
+#' summarize_dge(GSE161650_de)
 summarize_dge <- function(df, fdr_col = "FDR", lfc_col = "logFC", fdr = 0.05, lfc = 0) {
   up <- sum(df[[fdr_col]] < fdr & abs(df[[lfc_col]]) > lfc & df[[lfc_col]] > 0, na.rm = TRUE)
   down <- sum(df[[fdr_col]] < fdr & abs(df[[lfc_col]]) > lfc & df[[lfc_col]] < 0, na.rm = TRUE)
@@ -43,6 +21,9 @@ summarize_dge <- function(df, fdr_col = "FDR", lfc_col = "logFC", fdr = 0.05, lf
   data.frame(
     Direction = factor(c("Up", "Down", "Unperturbed"), levels = c("Up", "Down", "Unperturbed")),
     N = c(up, down, unperturbed),
-    Percent = c(up / total * 100, down / total * 100, unperturbed / total * 100)
+    Percent = c(round(up / total * 100, 2),
+                round(down / total * 100, 2),
+                round(unperturbed / total * 100, 2)
+                )
     )
 }
