db64b35903c46818558723e4ec9c254745096701
jcalendo <jcalendo@gmail.com>
2022-10-26 16:27:51 -0400

    Updated docs to use example data

--- R/plot-md.R
+++ R/plot-md.R
@@ -26,37 +26,7 @@
 #' @import data.table
 #' @export
 #' @examples
-#' \dontrun{
-#' library(edgeR)
-#' library(coriell)
-#'
-#' # create some fake data
-#' x <- data.frame(
-#'   ctl1 = rnbinom(1000, size = 0.4, prob = 1e-5),
-#'   ctl2 = rnbinom(1000, size = 0.4, prob = 1e-5),
-#'   trt1 = rnbinom(1000, size = 0.4, prob = 1e-5),
-#'   trt2 = rnbinom(1000, size = 0.4, prob = 1e-5),
-#'   row.names = paste0("gene", 1:1000)
-#' )
-#'
-#' # run edger pipeline
-#' group <- factor(c(1, 1, 2, 2))
-#' y <- DGEList(counts = x, group = group)
-#' y <- calcNormFactors(y)
-#' design <- model.matrix(~group)
-#' y <- estimateDisp(y, design)
-#'
-#' # To perform quasi-likelihood F-tests:
-#' fit <- glmQLFit(y, design)
-#' qlf <- glmQLFTest(fit, coef = 2)
-#'
-#' # convert the results object to a dataframe
-#' res_df <- edger_to_df(qlf)
-#'
-#' # Create md plot
-#' plot_md(res_df)
-#' }
-#'
+#' plot_md(GSE161650_de, fdr = 0.01, lfc = log2(2))
 plot_md <- function(df, x = "logCPM", y = "logFC", sig_col = "FDR", fdr = 0.1, 
                     lfc = 0, annotate_counts = TRUE, up_color = "red2", down_color = "royalblue2",
                     nonde_color = "grey40", up_alpha = 1, down_alpha = 1, nonde_alpha = 1,
