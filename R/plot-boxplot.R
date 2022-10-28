d140ba969d88883f4e734ce9181b1ea881688aa6
jcalendo <jcalendo@protonmail.com>
2022-10-28 08:58:12 -0400

    Always plot all samples on x-axis. Only change fill

--- R/plot-boxplot.R
+++ R/plot-boxplot.R
@@ -6,8 +6,8 @@
 #' @param metadata data.frame containing metadata per sample. rownames of metadata
 #' must match the colnames of the input matrix. Default NULL, each sample in the 
 #' matrix will be plotted.
-#' @param fillBy metadata column used to group and fill boxplots. Default NULL, 
-#' each sample in the matrix will be plotted.
+#' @param fillBy metadata column used to fill boxplots. Default NULL, 
+#' each sample will be a distinct color.
 #' @param ... Additional parameters passed to \code{ggplot2::geom_boxplot()}
 #' @import data.table
 #' @return ggplot object
@@ -21,7 +21,7 @@
 #' plot_boxplot(GSE161650_lc) + 
 #'   theme_coriell()
 #'   
-#' # Plot the boxplot by Group in metadata
+#' # Plot the boxplot by coloring each Group
 #' plot_boxplot(GSE161650_lc, metadata, fillBy = "Group") + 
 #'   theme_coriell()
 plot_boxplot <- function(x, metadata = NULL, fillBy = NULL, ...) {
@@ -46,7 +46,7 @@ plot_boxplot <- function(x, metadata = NULL, fillBy = NULL, ...) {
   if (is.null(fillBy)) 
     fillBy <- ".sample"
   
-  ggplot2::ggplot(dt.m, ggplot2::aes_string(x = fillBy, y = ".value")) +
+  ggplot2::ggplot(dt.m, ggplot2::aes_string(x = ".sample", y = ".value")) +
     ggplot2::geom_boxplot(ggplot2::aes_string(fill = fillBy), ...) +
     ggplot2::labs(
       x = NULL, 
