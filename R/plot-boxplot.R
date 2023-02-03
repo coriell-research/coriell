7f62bf5036f55d49e12aa687940fc41463d84c40
jcalendo <jcalendo@protonmail.com>
2023-02-02 21:45:34 -0500

    Added RLE and violin options

--- R/plot-boxplot.R
+++ R/plot-boxplot.R
@@ -1,14 +1,20 @@
 #' Create boxplot from an expression matrix
-#' 
-#' Create a boxplot of expression values for the given expression matrix. 
-#' 
+#'
+#' Create a boxplot (or violin plot) of expression distributions for the given 
+#' expression matrix. Optionally plot the relative log expression of the matrix.
+#'
 #' @param x gene by sample matrix or data.frame of numeric values to be plotted.
 #' @param metadata data.frame containing metadata per sample. rownames of metadata
-#' must match the colnames of the input matrix. Default NULL, each sample in the 
+#' must match the colnames of the input matrix. Default NULL, each sample in the
 #' matrix will be plotted.
-#' @param fillBy metadata column used to fill boxplots. Default NULL, 
+#' @param fillBy metadata column used to fill boxplots. Default NULL,
 #' each sample will be a distinct color.
-#' @param ... Additional parameters passed to \code{ggplot2::geom_boxplot()}
+#' @param rle Plot the relative log expression value. This option assumes
+#' that the input matrix has already been logged. Be sure to take the log of the 
+#' input matrix prior to setting this option.
+#' @param violin Plot the data as a violin plot instead of a boxplot. Default FALSE.
+#' @param ... Additional parameters passed to \code{ggplot2::geom_boxplot()} or
+#' \code{ggplot2::geom_violin()}
 #' @import data.table
 #' @return ggplot object
 #' @export
@@ -16,45 +22,66 @@
 #' # Create metadata for plotting
 #' metadata <- data.frame(row.names = colnames(GSE161650_lc))
 #' metadata$Group <- rep(c("DMSO", "THZ1"), each = 3)
-#' 
+#'
 #' # Plot the boxplot by sample
-#' plot_boxplot(GSE161650_lc) + 
+#' plot_boxplot(GSE161650_lc) +
+#'   theme_coriell()
+#'
+#' # Plot the boxplot by coloring each Group
+#' plot_boxplot(GSE161650_lc, metadata, fillBy = "Group") +
 #'   theme_coriell()
 #'   
-#' # Plot the boxplot by coloring each Group
-#' plot_boxplot(GSE161650_lc, metadata, fillBy = "Group") + 
+#' # Create a violin plot after RLE transformation
+#' plot_boxplot(GSE161650_lc, metadata, fillBy = "Group", rle = TRUE, violin = TRUE) +
 #'   theme_coriell()
-plot_boxplot <- function(x, metadata = NULL, fillBy = NULL, ...) {
+plot_boxplot <- function(x, metadata = NULL, fillBy = NULL, rle = FALSE, 
+                         violin = FALSE, ...) {
   stopifnot("colnames(x) do not match rownames(metadata)" = all(colnames(x) == rownames(metadata)))
   stopifnot("fillBy must be a column in metadata" = fillBy %in% colnames(metadata))
   stopifnot("non-numeric columns in x" = all(apply(x, 2, is.numeric)))
 
+  if (rle) {
+    m <- apply(x, 1, median, na.rm = TRUE)
+    x <- x - m
+  }
+
   dt <- data.table::as.data.table(x, keep.rownames = ".feature")
   dt.m <- data.table::melt(
-    dt, 
-    id.vars = ".feature", 
+    dt,
+    id.vars = ".feature",
     variable.name = ".sample",
     value.name = ".value",
     variable.factor = FALSE
-    )
-  
+  )
+
   if (!is.null(metadata)) {
     meta <- data.table::as.data.table(metadata, keep.rownames = ".sample")
     dt.m <- dt.m[meta, on = ".sample", nomatch = NULL]
   }
-  
-  if (is.null(fillBy))
+
+  if (is.null(fillBy)) {
     fillBy <- ".sample"
-  
+  }
+
   # Force x axis to be in Group order
   fct_levels <- dt.m[order(get(fillBy))][, unique(.sample)]
   dt.m[, .sample := factor(.sample, levels = fct_levels)]
-  
-  ggplot2::ggplot(dt.m, ggplot2::aes_string(x = ".sample", y = ".value")) +
-    ggplot2::geom_boxplot(ggplot2::aes_string(fill = fillBy), ...) +
+
+  p <- ggplot2::ggplot(dt.m, ggplot2::aes_string(x = ".sample", y = ".value"))
+
+  if (violin) {
+    p <- p + ggplot2::geom_violin(ggplot2::aes_string(fill = fillBy, ...))
+  } else {
+    p <- p + ggplot2::geom_boxplot(ggplot2::aes_string(fill = fillBy), ...)
+  }
+
+  p + ggplot2::geom_hline(
+    yintercept = median(x, na.rm = TRUE),
+    color = "red", linetype = 2
+  ) +
     ggplot2::labs(
-      x = NULL, 
-      y = NULL, 
+      x = NULL,
+      y = NULL,
       fill = if (fillBy == ".sample") "Sample" else fillBy
-      )
+    )
 }
