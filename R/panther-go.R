73417af46ff7ab4b2b2df5d0eef001e3fa659884
jcalendo <jcalendo@protonmail.com>
2023-07-27 19:12:48 -0400

    panther_go is back

--- R/panther-go.R
+++ R/panther-go.R
@@ -1,16 +1,16 @@
 #' Perform GO analysis with PANTHER
 #'
 #' This statistical test tool, compares a test gene list to a reference gene list,
-#' and determines whether a particular class (e.g. molecular function, biological 
-#' process, cellular component, PANTHER protein class, the PANTHER pathway or 
+#' and determines whether a particular class (e.g. molecular function, biological
+#' process, cellular component, PANTHER protein class, the PANTHER pathway or
 #' Reactome pathway) of genes is overrepresented or underrepresented.
-#' 
+#'
 #' @details
 #' Sends a request to \href{http://pantherdb.org/}{PANTHER db} to perform over
 #' representation analysis. This function excludes the option to import a reference
 #' list and reference organism. By default, in this case, PANTHER will use all
 #' of the genes of the given organism as the reference list.
-#' 
+#'
 #' @param gene_list character vector. Maximum of 100,000 identifiers. Can be any
 #' of the following: Ensemble gene identifier, Ensemble protein identifier,
 #' Ensemble transcript identifier, Entrez gene id, gene symbol, NCBI GI, HGNC Id,
@@ -43,13 +43,8 @@
 #'   "RBPJ", "SKP2", "TCF7", "TP53", "WNT1", "WNT5B", "WNT6"
 #' )
 #'
-#' go_results <- panther_go(genes, organism = "9606", annot_dataset = "biological_process")
-#'
-#' # to view results access the 'table' item
-#' head(go_results$table)
-#'
-#' # to view the actual request (which allows you to parse results yourself) use
-#' req <- go_results$request
+#' result <- panther_go(genes, "9606", "biological_process")
+#' head(result)
 panther_go <- function(gene_list,
                        organism,
                        annot_dataset,
@@ -58,10 +53,7 @@ panther_go <- function(gene_list,
   if (!requireNamespace("httr2", quietly = TRUE)) {
     stop("httr2 package is required.")
   }
-  if (!requireNamespace("jsonlite", quietly = TRUE)) {
-    stop("jsonlite package is required.")
-  }
-  
+
   datasets <- c(
     "biological_process" = "GO:0008150",
     "molecular_function" = "GO:0003674",
@@ -75,14 +67,14 @@ panther_go <- function(gene_list,
   )
   tests <- c("fisher" = "FISHER", "binomial" = "BINOMIAL")
   corrections <- c("fdr" = "FDR", "bonferroni" = "BONFERRONI", "none" = "NONE")
-  
+
   stopifnot("Empty gene_list" = length(gene_list) > 0)
   stopifnot("Too many genes in gene list" = length(gene_list) <= 100000)
   stopifnot("Only one organism identifier should be provided" = length(organism) == 1)
   stopifnot("Incorrectly specified dataset" = annot_dataset %in% names(datasets))
   stopifnot("enrichment_test_type must be one of c('fisher', 'binomial')" = enrichment_test_type %in% names(tests))
   stopifnot("correction must be one of c('fdr', 'bonferroni', 'none')" = correction %in% names(corrections))
-  
+
   base_url <- "https://pantherdb.org/services/oai/pantherdb/enrich/overrep?"
   gene_input <- paste(gene_list, collapse = ",")
   organism_input <- organism
@@ -96,20 +88,20 @@ panther_go <- function(gene_list,
     enrichmentTestType = test_input,
     correction = correction_input
   )
-  
-  req <- httr2::request(base_url)
-  req <- httr2::req_body_json(req, data)
-  return(req)
-  resp <- httr2::req_perform(req)
-  
-  
-  if (isTRUE(httr2::resp_is_error(resp)))
+
+  resp <- httr2::request(base_url) |>
+    httr2::req_method("POST") |>
+    httr2::req_url_query(!!!data) |>
+    httr2::req_headers(Accept = "application/json") |>
+    httr2::req_options(ssl_verifypeer = 0) |>
+    httr2::req_perform()
+
+  if (isTRUE(httr2::resp_is_error(resp))) {
     stop("An error occured in the request")
-  
+  }
+
   json <- httr2::resp_body_json(resp)
-  return(json)
-  parsed <- jsonlite::fromJSON(json, simplifyVector = FALSE)
-  dt <- data.table::rbindlist(parsed$results$result)
-  
+  dt <- data.table::rbindlist(json$results$result)
+
   return(dt)
 }
