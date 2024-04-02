e0e674fed0f6fd7fd74704f4ae0d4e45943a4c6f
jcalendo <jcalendo@pm.me>
2024-04-02 10:05:07 -0400

    Increase font sizes and simplify strip text. Change default font

--- R/themes.R
+++ R/themes.R
@@ -9,7 +9,7 @@
 #' df <- data.frame(
 #'   X = 1:100,
 #'   Y = rnorm(100),
-#'   C = sample(LETTERS[1:4], 100, replace = TRUE)
+#'   C = sample(c("Group 1", "Group 2", "Group 3"), 100, replace = TRUE)
 #' )
 #'
 #' # Default ggplot2
@@ -17,27 +17,43 @@
 #'   geom_point() +
 #'   labs(
 #'     title = "ggplot2 default plot",
-#'     subtitle = "subtitle for default plot"
+#'     subtitle = "subtitle for default plot",
+#'     x = "X Label",
+#'     y = "Y Label"
 #'   )
 #'
 #' # With theme_coriell()
 #' ggplot(df, aes(X, Y, color = C)) +
 #'   geom_point() +
 #'   labs(
-#'     title = "Plot with theme_coriell()",
-#'     subtitle = "Subtitle for plot with theme_coriell()"
+#'     title = "Plot using theme_coriell()",
+#'     subtitle = "Show facets and plot legend",     
+#'     x = "X Label",
+#'     y = "Y Label"
 #'   ) +
+#'   facet_wrap(~C) +
 #'   theme_coriell()
 #'
 theme_coriell <- function() {
   ggplot2::theme_light() +
     ggplot2::theme(
+
+      text = ggplot2::element_text(family = "AvantGarde"),
+      
       legend.position = "bottom",
+      legend.title = ggplot2::element_text(size = 16, face = "bold"),
+      legend.text = ggplot2::element_text(size = 14),
+      
       plot.title = ggplot2::element_text(size = 22, face = "bold"),
-      plot.subtitle = ggplot2::element_text(size = 16),
-      axis.title = ggplot2::element_text(size = 14),
-      strip.text = ggplot2::element_text(size = 12, face = "bold"),
-      axis.text.x = ggplot2::element_text(size = 14, angle = 45, hjust = 1),
-      axis.text.y = ggplot2::element_text(size = 14)
+      plot.subtitle = ggplot2::element_text(size = 18),
+      
+      axis.title = ggplot2::element_text(size = 16),
+      axis.text.x = ggplot2::element_text(size = 16, angle = 45, hjust = 1),
+      axis.text.y = ggplot2::element_text(size = 16),
+      
+      strip.text.x = ggplot2::element_text(size = 16, color = "black", face = "bold"),
+      strip.text.y = ggplot2::element_text(size = 16, color = "black", face = "bold"),
+      strip.background = ggplot2::element_rect(fill = "white", linewidth = 2)
+      
     )
 }
