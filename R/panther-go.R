75e787dfb72c38d84882d3624d65388a9098167d
jcalendo <jcalendo@pm.me>
2024-06-19 15:53:03 -0400

    Add reference list argument

--- R/panther-go.R
+++ R/panther-go.R
@@ -19,6 +19,8 @@
 #' @param organism character string. Taxon ID (e.g. "9606" for HUMAN, "10090" for
 #' MOUSE, "10116" for RAT). To get list of available taxon IDs see:
 #' \preformatted{curl -X GET "https://pantherdb.org/services/oai/pantherdb/supportedgenomes" -H  "accept: application/json"}
+#' @param ref_input_list Reference set of genes for the specified organism. If 
+#' NULL (default) then PANTHER will use all genes for the specified organism. 
 #' @param annot_dataset character string. One of c("biological_process",
 #' "molecular_function", "cellular_component", "panther_go_slim_mf", "panther_go_slim_bp",
 #' "panther_go_slim_cc", "panther_pc", "panther_pathway", "panther_reactome_pathway"). see:
@@ -48,8 +50,10 @@
 panther_go <- function(gene_list,
                        organism,
                        annot_dataset,
+                       ref_input_list = NULL,
                        enrichment_test_type = "fisher",
-                       correction = "fdr") {
+                       correction = "fdr",
+                       verbose = 0) {
   if (!requireNamespace("httr2", quietly = TRUE)) {
     stop("httr2 package is required.")
   }
@@ -81,6 +85,7 @@ panther_go <- function(gene_list,
   annot_input <- unname(datasets[annot_dataset])
   test_input <- unname(tests[enrichment_test_type])
   correction_input <- unname(corrections[correction])
+  
   data <- list(
     geneInputList = gene_input,
     organism = organism_input,
@@ -88,15 +93,22 @@ panther_go <- function(gene_list,
     enrichmentTestType = test_input,
     correction = correction_input
   )
-
+  
+  if (!is.null(ref_input_list)) {
+    stopifnot("Reference input list must be a character vector" = is.character(ref_input_list))
+    stopifnot("Reference input list must be <= 100,000 identifiers" = length(ref_input_list) <= 100000)
+    ref_list <- paste(ref_input_list, collapse = ",")
+    data$refInputList <- ref_list
+    data$refOrganism <- data$organism
+  }
+  
   resp <- httr2::request(base_url) |>
     httr2::req_user_agent("coriell (https://coriell-research.github.io/coriell)") |> 
     httr2::req_method("POST") |>
-    httr2::req_url_query(!!!data) |>
+    httr2::req_body_form(!!!data) |>
     httr2::req_headers(Accept = "application/json") |> 
-    httr2::req_options(ssl_verifypeer = 0) |>  # This could be dangerous...
-    httr2::req_perform()
-
+    httr2::req_perform(verbosity = verbose)
+  
   if (isTRUE(httr2::resp_is_error(resp))) {
     stop("An error occured in the request")
   }
