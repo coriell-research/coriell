2c5887e20cee0fe62d2732a32362112b45e579d4
jcalendo <jcalendo@gmail.com>
2024-06-26 14:58:10 -0400

    Get limits in the case when coords have been modified

--- R/get-axis-limits.R
+++ R/get-axis-limits.R
@@ -6,14 +6,29 @@
 #' @export
 #' @return list with elements for x and y limits
 get_axis_limits <- function(p) {
+  
   b <- ggplot2::ggplot_build(p)
+  
   x_range <- b$layout$panel_scales_x[[1]]$range$range
   y_range <- b$layout$panel_scales_y[[1]]$range$range
-
   xmin <- x_range[1]
   xmax <- x_range[2]
   ymin <- y_range[1]
   ymax <- y_range[2]
+  
+  # Get limits if the ranges have been reset by coord_
+  x_limits <- b$layout$coord$limits$x
+  y_limits <- b$layout$coord$limits$y
+  
+  if (!is.null(x_limits)) {
+    xmin <- x_limits[1]
+    xmax <- x_limits[2]
+  }
+
+  if (!is.null(y_limits)) {
+    ymin <- y_limits[1]
+    ymax <- y_limits[2]
+  }
 
   list(
     "x_min" = xmin,
