4eb944bb6b7c80f9b437894c638bf8b6f4df2bb5
jcalendo <jcalendo@pm.me>
2024-10-18 18:57:31 -0400

    Remove subsampling and allow unstranded ranges. Add option to return a matrix of counts

--- R/pileup-profile.R
+++ R/pileup-profile.R
@@ -16,43 +16,42 @@
 #' Return the pileup of reads over a set of GRanges
 #' 
 #' This function computes pileups for BAM files over a set of GRanges. It 
-#' returns sum and average counts for all positions relative to the 5' end of 
-#' the ranges. This function also allows for subsampling of ranges to speed up
-#' calculations. 
+#' returns pileup stats relative to the 5' to 3' direction taking strandedness 
+#' of the input range into account. 
 #'
 #' @param bamfile Path to an indexed BAM file
 #' @param x GRanges to calculate pileups over
 #' @param scan_bam_flag An instance of Rsamtools::scanBamFlag() used to to create 
 #' a parameter object influencing what fields and which records are imported 
 #' from a (binary) BAM file
-#' @param sample_size Number of records ranges to be randomly subsampled from x. 
-#' Default NULL, no subsampling.
-#' @param max_depth integer; maximum number of overlapping alignments considered 
+#' @param max_depth maximum number of overlapping alignments considered 
 #' for each position in the pileup. See Rsamtools::PileupParam()
-#' @param min_base_quality integer; minimum ‘QUAL’ value for each nucleotide 
+#' @param min_base_quality minimum ‘QUAL’ value for each nucleotide 
 #' in an alignment. Use phred2ASCIIOffset to help translate numeric or 
 #' character values to these offsets.
-#' @param min_mapq integer; minimum ‘MAPQ’ value for an alignment to be included 
+#' @param min_mapq minimum ‘MAPQ’ value for an alignment to be included 
 #' in pileup.
-#' @param min_nucleotide_depth integer(1); minimum count of each nucleotide 
+#' @param min_nucleotide_depth minimum count of each nucleotide 
 #' (independent of other nucleotides) at a given position required for said 
 #' nucleotide to appear in the result.
+#' @param return_matrix Should the function return a range by position matrix
+#' containing the pileup counts at each relative position? If FALSE (default) 
+#' then a data.table of summary stats at each relative position is returned.
 #' 
 #' @details
-#' This function takes in a BAM file and computes a pileup over the given ranges. 
-#' The data.table which gets returned contains the sum and average count of 
-#' reads at each position relative to the 5' end of the input ranges. The user 
-#' can also randomly subsample the input ranges to speed up computation. 
-#' For both subsampling and pileup computation, ranges are split by positive and 
-#' negative strand, subsampled at the same depth (if set), and positions in the 
-#' final data.table are returned relative to the 5' start in the input ranges.
+#' For pileup computation, ranges are split by positive and negative strand 
+#' and positions in the final data.table are returned relative to the 5' start
+#' in the input ranges. If unstranded ranges are supplied then they are treated 
+#' as positively stranded ranges. The function can also return a range x 
+#' relative_position matrix containing pileup counts at each position if 
+#' return_matrix=TRUE.  
 #' 
-#' @return data.table
+#' @return data.table or matrix
 #' @import data.table
 #' @export
-pileup_profile <- function(bamfile, x, scan_bam_flag, sample_size = NULL,
-                           max_depth = 1e4, min_base_quality = 13, min_mapq = 1,
-                           min_nucleotide_depth = 0) {
+pileup_profile <- function(bamfile, x, scan_bam_flag, max_depth = 1e4, 
+                           min_base_quality = 13, min_mapq = 1,
+                           min_nucleotide_depth = 0, return_matrix = FALSE) {
   
   if (!requireNamespace("Rsamtools", quietly = TRUE)) {
     stop("Rsamtools is required")
@@ -70,35 +69,44 @@ pileup_profile <- function(bamfile, x, scan_bam_flag, sample_size = NULL,
     distinguish_strands = FALSE
   )
   
-  if (all(GenomicRanges::strand(x) == "*")) {
-    stop("Unstranded ranges are not yet supported!")
+  if (any(GenomicRanges::strand(x) == "*")) {
+    message("Unstranded ranges detected. These will be treated as positively stranded.")
   }
-
-  pos_gr <- x[GenomicRanges::strand(x) == "+"]
-  neg_gr <- x[GenomicRanges::strand(x) == "-"]
-
-  if (!is.null(sample_size)) {
-    n <- floor(sample_size / 2)
-    if (n > length(pos_gr) || n > length(neg_gr)) {
-      n <- min(c(length(pos_gr), length(neg_gr)))
-      message("subsample size > length of ranges on either strand. Setting n to ", n)
-    }
-    pos_gr <- pos_gr[sample.int(length(pos_gr), size = n)]
-    neg_gr <- neg_gr[sample.int(length(neg_gr), size = n)]
-  }
-
-  pos_bparam <- Rsamtools::ScanBamParam(flag = scan_bam_flag, which = pos_gr)
-  neg_bparam <- Rsamtools::ScanBamParam(flag = scan_bam_flag, which = neg_gr)
-
-  pp <- .pileup2dt(Rsamtools::pileup(bamfile, scanBamParam = pos_bparam, pileupParam = pparam))
-  np <- .pileup2dt(Rsamtools::pileup(bamfile, scanBamParam = neg_bparam, pileupParam = pparam))
   
-  # Start counting from the 5' end
-  pp[, relative_position := pos - start]
-  np[, relative_position := end - pos]
-  df <- rbind(pp, np)
-
-  result <- df[, .(n = .N, total = sum(count), avg = mean(count)), by = relative_position]
+  pos_gr <- x[GenomicRanges::strand(x) == "+" | GenomicRanges::strand(x) == "*"]
+  neg_gr <- x[GenomicRanges::strand(x) == "-"]
+  
+  if (length(pos_gr) != 0) {
+    pos_bparam <- Rsamtools::ScanBamParam(flag = scan_bam_flag, which = pos_gr)
+    pp <- .pileup2dt(Rsamtools::pileup(bamfile, scanBamParam = pos_bparam, pileupParam = pparam))
+    pp[, relative_position := pos - start]
+  } else {
+    pp <- data.table::data.table()
+  }
+  
+  if (length(neg_gr) != 0) {
+    neg_bparam <- Rsamtools::ScanBamParam(flag = scan_bam_flag, which = neg_gr)
+    np <- .pileup2dt(Rsamtools::pileup(bamfile, scanBamParam = neg_bparam, pileupParam = pparam))
+    np[, relative_position := end - pos]
+  } else {
+    np <- data.table::data.table()
+  }
+  
+  dt <- rbind(pp, np)
+  
+  if (isTRUE(return_matrix)) {
+    m <- data.table::dcast(
+      dt, 
+      which_label ~ relative_position, 
+      value.var = "count", 
+      fill = 0L
+      )
+    m <- as.matrix(m, rownames = "which_label")
+    
+    return(m)
+  }
+  
+  result <- dt[, .(n = .N, total = sum(count), avg = mean(count)), by = relative_position]
   result[, normalized := coriell::minmax(avg)]
   data.table::setorder(result, relative_position)
 
